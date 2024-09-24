e2543b9e28c9a732f424d48f76911b62e1844dfd
Yeabsira Legesse <YEABSIRA1202@GMAIL.COM>
2024-09-24 13:50:33 -0400

    finished the details screen

---
+++ lib/details_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget{
+  final Map<String, String> recipe;
+
+  const DetailsScreen({super.key, required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(recipe['name']!),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Ingredients: ',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(height: 8),
+            Text(recipe['ingredients']!),
+            const SizedBox(height: 16),
+            const Text(
+              'Instructions: ',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(height: 8),
+            Text(recipe['instructions']!),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
